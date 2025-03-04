e882c4f03c1c8ad55bd722e76d1c376831c37574
Johannes Pfau <johannespfau@gmail.com>
2025-03-04 21:43:57 +0100

    Create the Wishbone driver earlier

--- test/test.py
+++ test/test.py
@@ -13,12 +13,6 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.rstn.value = 0
-    await ClockCycles(dut.clk, 3)
-    dut.rstn.value = 1
-
     # FIXME: reg[3:0] wb_sel_i, wirewb_err_o
     wbs = WishboneMaster(dut, "", dut.clk,
         width=32,
@@ -31,6 +25,11 @@ async def test_project(dut):
                     "datrd":"wb_dat_o",
                     "ack":  "wb_ack_o" })
 
-    await wbs.send_cycle([WBOp(0, 0xabcd1234), WBOp(1, 0x1bcd1234)])
+    # Reset
+    dut._log.info("Reset")
+    dut.rstn.value = 0
+    await ClockCycles(dut.clk, 3)
+    dut.rstn.value = 1
 
+    await wbs.send_cycle([WBOp(0, 0xabcd1234), WBOp(1, 0x1bcd1234)])
     await ClockCycles(dut.clk, 3)
