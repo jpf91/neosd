d45b6e0cfa676e06f5cae73e3410448d7e8685fe
Johannes Pfau <johannespfau@gmail.com>
2025-08-30 11:05:42 +0200

    Update test bench for interface changes

--- test/tb.v
+++ test/tb.v
@@ -11,7 +11,6 @@ module tb ();
 
     reg clk;
     reg rstn;
-    reg[7:0] clk_gen;
 
     reg[31:0] wb_adr_i;
     reg[31:0] wb_dat_i;
@@ -35,7 +34,6 @@ module tb ();
     neosd dut (
         .clk_i(clk),
         .rstn_i(rstn),
-        .clkgen_i(clk_gen),
     
         .wb_adr_i(wb_adr_i),
         .wb_dat_i(wb_dat_i),
@@ -45,7 +43,6 @@ module tb ();
         .wb_cyc_i(wb_cyc_i),
     
         .wb_ack_o(wb_ack_o),
-        .wb_err_o(wb_err_o),
         .wb_dat_o(wb_dat_o),
 
         .sd_clk_o(sd_clk_o),
@@ -65,25 +62,5 @@ module tb ();
         .sd_dat2_oe(sd_dat2_oe),
         .sd_dat3_oe(sd_dat3_oe)
     );
-
-    reg[11:0] cnt, cnt2;
-    always @(posedge clk or negedge rstn) begin
-        if (rstn == 0) begin
-            cnt <= 0;
-            cnt2 <= 0;
-        end else begin
-            cnt <= cnt + 1;
-            cnt2 <= cnt;
-        end
-    end
-
-    assign clk_gen[0] = cnt[0]  & !cnt2[0];  // clk_i / 2
-    assign clk_gen[1] = cnt[1]  & !cnt2[1];  // clk_i / 4
-    assign clk_gen[2] = cnt[2]  & !cnt2[2];  // clk_i / 8
-    assign clk_gen[3] = cnt[5]  & !cnt2[5];  // clk_i / 64
-    assign clk_gen[4] = cnt[6]  & !cnt2[6];  // clk_i / 128
-    assign clk_gen[5] = cnt[9]  & !cnt2[9];  // clk_i / 1024
-    assign clk_gen[6] = cnt[10] & !cnt2[10]; // clk_i / 2048
-    assign clk_gen[7] = cnt[11] & !cnt2[11]; // clk_i / 4096
 
 endmodule
